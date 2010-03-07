1e85aab159435a24868c7bf28c54792db209086d
iluxa13 <iluxa13@9f77efb2-2a0e-11df-a33e-39b8177f566d>
2010-03-07 20:29:22 +0000

    hey!

--- americanoforum/src/forum/server/domainlayer/User.java
+++ americanoforum/src/forum/server/domainlayer/User.java
@@ -4,9 +4,14 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class User {
-	UserPermission _up;
-	Details _details;
-	HashMap<Integer, Message> _myMessages = new HashMap<Integer, Message>();
+	private UserPermission _up;
+	private Details _details;
+	private HashMap<Integer, Message> _myMessages = new HashMap<Integer, Message>();
+
+    public User() {
+        this._up = GuestPermission.getInstance();
+        this._details=null;
+    }
 
 	public void addMessage(String aSbj, String aCont) {
 		throw new UnsupportedOperationException();
@@ -23,4 +28,26 @@ public class User {
 	public void reply(Message aParent_msg, String aSbj, String aCont) {
 		throw new UnsupportedOperationException();
 	}
+
+        public Details getDetails() {
+        return _details;
+    }
+
+    public void setDetails(Details _details) {
+        this._details = _details;
+    }
+
+    public void setUp(UserPermission _up) {
+        this._up = _up;
+    }
+
+    public HashMap<Integer, Message> getMyMessages() {
+        return _myMessages;
+    }
+
+    public UserPermission getUp() {
+        return _up;
+    }
+    
+
 }
