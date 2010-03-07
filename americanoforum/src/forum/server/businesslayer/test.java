eb40f60ac8d4e545858549f7f1c90d7e7e06c45a
iluxa13 <iluxa13@9f77efb2-2a0e-11df-a33e-39b8177f566d>
2010-03-07 18:14:06 +0000

    testing

---
+++ americanoforum/src/forum/server/businesslayer/test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package forum.server.businesslayer;
+
+/**
+ *
+ * @author Ilya
+ */
+public class test {
+
+}
