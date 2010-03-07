36798fac25e8670987e3a2efe3ddc95cb910c295
iluxa13 <iluxa13@9f77efb2-2a0e-11df-a33e-39b8177f566d>
2010-03-07 20:43:10 +0000

    hey!

--- americanoforum/src/forum/server/domainlayer/LoggedInPermission.java
+++ americanoforum/src/forum/server/domainlayer/LoggedInPermission.java
@@ -1,24 +1,39 @@
 package forum.server.domainlayer;
 
 public class LoggedInPermission implements UserPermission {
+    
+ private static LoggedInPermission instance=null;
 
 	public void addMessage(String aSbj, String aCont) {
-		throw new UnsupportedOperationException();
+            return;
 	}
 
 	public void modifyMessage(Message aMsg, String aCont) {
-		throw new UnsupportedOperationException();
+		return;
 	}
 
 	public void viewMessage(Message aMsg) {
-		throw new UnsupportedOperationException();
+	    return;
 	}
 
 	public void reply(Message aParent_msg, String aSbj, String aCont) {
-		throw new UnsupportedOperationException();
+		return;
 	}
 
-	public UserPermission getInstance() {
-		throw new UnsupportedOperationException();
+	public static UserPermission getInstance() {
+		if (getInstanceField()==null){
+                    setInstance(new LoggedInPermission());
+                    return getInstanceField();
+                }
+                else
+                    return getInstanceField();
 	}
+
+    public static LoggedInPermission getInstanceField() {
+        return instance;
+    }
+
+    public static void setInstance(LoggedInPermission instance) {
+        LoggedInPermission.instance = instance;
+    }
 }
